8395139ef9d094309811a150cbbd51a1476ddeec
Ashley Thompson <ashleythompsonbca@gmail.com>
2020-05-12 23:23:52 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,7 +10,7 @@ def introduction(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction (name, language=Ruby)
+def introduction (name, language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
